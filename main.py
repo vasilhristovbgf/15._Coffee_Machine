b887dc6ed0da1d1bf5d9b00bd44b28d951e0914a
VasilHristov <vasil.hristovbgf@gmail.com>
2023-01-13 14:17:46 +0100

    Started Coffee Machine project

--- main.py
+++ main.py
@@ -1,1 +1,21 @@
-print("hello world!")
+import menu
+
+print("What would you like to drink?")
+order = input("1. Espresso || 2. Latte || 3. Cappuccino ")
+def espresso():
+    water = menu.MENU["espresso"]["ingredients"]["water"]
+    coffee = menu.MENU["espresso"]["ingredients"]["coffee"]
+
+def latte():
+    water = menu.MENU["latte"]["ingredients"]["water"]
+    coffee = menu.MENU["latte"]["ingredients"]["coffee"]
+    milk = menu.MENU["latte"]["ingredients"]["milk"]
+
+def cappuccino():
+    water = menu.MENU["cappuccino"]["ingredients"]["water"]
+    coffee = menu.MENU["cappuccino"]["ingredients"]["coffee"]
+    milk = menu.MENU["cappuccino"]["ingredients"]["milk"]
+
+
+
+print(menu.MENU["espresso"]["ingredients"]["water"])
