10b74932ac2f7e63d97434d20ff4a821c1f6d349
VasilHristov <vasil.hristovbgf@gmail.com>
2023-01-13 15:21:38 +0100

    Simplified some code

--- main.py
+++ main.py
@@ -1,21 +1,29 @@
 import menu
 
 print("What would you like to drink?")
-order = input("1. Espresso || 2. Latte || 3. Cappuccino ")
-def espresso():
-    water = menu.MENU["espresso"]["ingredients"]["water"]
-    coffee = menu.MENU["espresso"]["ingredients"]["coffee"]
-
-def latte():
-    water = menu.MENU["latte"]["ingredients"]["water"]
-    coffee = menu.MENU["latte"]["ingredients"]["coffee"]
-    milk = menu.MENU["latte"]["ingredients"]["milk"]
-
-def cappuccino():
-    water = menu.MENU["cappuccino"]["ingredients"]["water"]
-    coffee = menu.MENU["cappuccino"]["ingredients"]["coffee"]
-    milk = menu.MENU["cappuccino"]["ingredients"]["milk"]
+usrOrder = input("espresso || latte || cappuccino ")
 
 
+def drink(order):
+    if order == "report":
+        print(menu.resources)
+    else:
+        water = menu.MENU[order]["ingredients"]["water"]
+        coffee = menu.MENU[order]["ingredients"]["coffee"]
+        if order != "espresso":
+            milk = menu.MENU[order]["ingredients"]["milk"]
+        else:
+            milk = 0
+
+        if menu.resources["water"] > water:
+            if menu.resources["coffee"] > coffee:
+                if menu.resources["milk"] > milk:
+                    menu.resources["water"] -= water
+                    menu.resources["coffee"] -= coffee
+                    menu.resources["milk"] -= milk
+                    print("Drink is ready")
+
+
+drink(usrOrder)
 
 print(menu.MENU["espresso"]["ingredients"]["water"])
